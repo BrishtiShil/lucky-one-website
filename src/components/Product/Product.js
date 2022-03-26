b726caa142c04306c0d4d1d2313a1983e4d93462
Brishti Shil <brishtishil62@gmail.com>
2022-03-26 10:58:10 +0600

    fourth commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
@@ -11,7 +13,8 @@ const Product = (props) => {
             <p className='product-name'>{name}</p>
             <p>Price: ${price}</p>
             <button onClick={() => handleAddToCart(gun)} className='btn-cart'>
-                <p>Add to cart</p>
+                <p className='btn-text'>Add to cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
